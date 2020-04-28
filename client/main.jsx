3a97a2447beb6dc10d8e3a79fedb7dd2ef5ed4fe
Marcin Krawczyk <marcinn.krawczyk@gmail.com>
2020-04-28 15:20:33 +0200

    Test fixes.

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,13 @@
-import { App } from '../ui/App';
+import React from 'react';
+import { Header, App } from '../ui/App';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { mount } from 'react-mounter';
+import { render } from 'react-dom';
+
+Meteor.startup(() => {
+  // This could be removed and put inside App.jsx but I left it here so test will see H1 component immediately.
+  render(<Header />, document.getElementById('app'));
+});
 
 FlowRouter.route('/', {
   name: 'index',
--- ui/App.jsx
+++ ui/App.jsx
@@ -10,6 +10,8 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import PropTypes from 'prop-types';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
+export const Header = () => <h1>{TEXTS.HOME_TITLE}</h1>;
+
 /**
  * Represents may layout of app.
  * Fetches Communities collection and passes it to Select Event selector.
@@ -53,8 +55,6 @@ export const App = ({ eventId = '' }) => {
 
   return (
     <div>
-      <h1>{TEXTS.HOME_TITLE}</h1>
-
       <SelectEvent
         options={options}
         selected={eventId}
